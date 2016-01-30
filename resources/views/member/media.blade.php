18e704f0445cdbf25bae2fc6e1b3c0093314e0ae
ferdie de oliveira <ferdie de oliveira>
2016-01-30 15:04:56 +0000

    Added Modal.

--- resources/views/member/media.blade.php
+++ resources/views/member/media.blade.php
@@ -8,8 +8,8 @@
 </form>
 <div class="row-flud">
 @foreach($images as $image)
-  <div class="col-md-1 iCon" data-imd="{{$image->id}}">
-   <div class="img-op">
+  <div class="col-md-1 iCon" data-imd="{{$image->id}}" id="{{$image->id}}mediaImage">
+   <div class="img-op data-del" data-del="{{$image->id}}">
      <i class="halflings-icon white trash im"></i>
    </div>
     <img width="200" src="{{$image->imagePath}}/{{$image->imageName}}" alt="{{$image->imageName}}" data-image="{{$image->imageName}}">
@@ -17,5 +17,24 @@
 @endforeach
 </div>
 
+<div id="myModalEdit" class="modal fade" role="dialog">
+  <div class="modal-dialog">
 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Confirm Delete</h4>
+      </div>
+      <div class="modal-body">
+        <textarea class="updateTextEdit"></textarea>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-default danger" data-dismiss="modal">Delete</button>
+      </div>
+    </div>
+
+  </div>
+</div>
 @endsection
