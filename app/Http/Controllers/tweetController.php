79deb29bec2592b5918a27c41077d767b1c6dc62
ferdie de oliveira <ferdie de oliveira>
2016-02-02 15:43:49 +0000

    Removed DB and added eliquent

--- app/Http/Controllers/tweetController.php
+++ app/Http/Controllers/tweetController.php
@@ -5,8 +5,10 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Post;
 use DB;
 use Auth;
+use Log;
 class tweetController extends Controller
 {
 
@@ -24,6 +26,20 @@ class tweetController extends Controller
         $postTime     = $_POST['time'];
         $timeZone     = $_POST['timeZone'];
         $tweetMessage = $_POST['tweetMessage'];
+		  if(is_null($_POST['img']) || strlen ( $_POST['img']) == 0 ){
+
+            $image = 0;
+            $im = false;
+            $message = "Tweet created";
+		  }else{
+            //var_dump($_POST['img']);
+            $img = $_POST['img'];
+            $im = true;
+            $image = 1;
+            $message = "Tweet Created with image";
+
+		  }
+
 
         //Check if all fields are filled in
 
@@ -33,6 +49,7 @@ class tweetController extends Controller
 
           if(strlen($tweetMessage) > 140){
             return "Please your tweet length is grater than 140 characters";
+            die();
           }
 
         $clienID = DB::table('clients')->where('clientName', $clientName)->where('userID', $memberID)->first();
@@ -40,20 +57,34 @@ class tweetController extends Controller
         //check tweet message length. NEED TO COMPLETE THIS.
         // save the tweet
         //Insert into DB
-        DB::table('posts')->insert(
-            [
-              'clientID' => $clienID, //GET CLEINT ID
-              'userID' => $memberID,
-              'timeZone' => $timeZone,
-              'time' => $postTime,
-              'date' => $postDate,
-              'message' => $tweetMessage,
-              'status' => '1',
-              'platform' => '0'
-            ]
-        );
+          $savePost = new Post();
+          $savePost->clientID = $clienID;
+          $savePost->userID = $memberID;
+          $savePost->timeZone = $timeZone;
+          $savePost->time = $postTime;
+          $savePost->date = $postDate;
+          $savePost->message = $tweetMessage;
+          $savePost->status = 1;
+          $savePost->platform = 0;
+          $savePost->image = $image;
+          $savePost->save();
 
-        echo "Shedule Tweet Created!";
+
+        if($im == true){
+          $imgs = json_decode($img);
+            foreach ($imgs as $imageID){
+               DB::table('imagesToSends')->insert(
+                [
+                  'userID' => $memberID, //GET CLEINT ID
+                  'postID' => $savePost->id,
+                  'imageID' => $imageID
+                ]
+              );
+            }
+        }
+
+
+        echo $message;
         }
       }else{
         echo "Please fill in all mandatory fields.";
@@ -62,55 +93,40 @@ class tweetController extends Controller
 
     //METHOD TO RETURN POST MESASGE by id
 
-    public function getPostMessage(){
-      $postID = $_GET['postID'];
-      return DB::table('posts')->where('id', $postID)->value('message');
-    }
+  public function getPostMessage()
+  {
+    $postID = $_GET['postID'];
+    return DB::table('posts')->where('id', $postID)->value('message');
+  }
 
-    public function destroy()
-    {
-      $postID = $_POST['postID'];
-      $user = Auth::user();
-      $memberID = $user->id;
-        //FUNCTION TO DELETE A POST
-      DB::table('posts')->where('id', $postID)->where("userID" , $memberID)->delete();
-      echo "Post is now deleted";
-    }
+  public function destroy()
+  {
+    $postID = $_POST['postID'];
+    $user = Auth::user();
+    $memberID = $user->id;
+      //FUNCTION TO DELETE A POST
+    DB::table('posts')->where('id', $postID)->where("userID" , $memberID)->delete();
+    //Delete all images to send if any
+    DB::table('imagesToSends')->where('postID', $postID)->delete();
+    echo "Post is now deleted";
+  }
 
   //EDIT POST
 
-  public function editPost(){
+  public function editPost()
+  {
     $postID = $_POST['postID'];
     return DB::table('posts')->where('id', $postID)->value('message');
   }
 
-  public function sendCronJobs(){
-    $posts = DB::table('posts')->where('status', '1')->get();
-
-    foreach($posts as $post){
-      $clientID = $post->clientID;
-      //Get oAuth
-
-      $oauth = DB::table('twitterOAuth')->where('clientID', $clientID)->get();
-
-      foreach($oauth as $auth){
-
-        $publicAuth = $auth->oauth_token;
-        $authSecret = $auth->oauth_token_secret;
-        \Codebird\Codebird::setConsumerKey('jtB9eFeOc1iHYN08GIXuZIZtx','09JZMkC0y55Czp0GRUnYufqka1zorPVFiyqdhVqAtWzApoNsof');
-        $twitterClient = \Codebird\Codebird::getInstance();
-        //Send the message
-          $twitterClient->setToken($publicAuth, $authSecret);
-          $params = array(
-            'status' => 'Hello @veebowen'
-          );
-          $reply = $twitterClient->statuses_update($params);
-      }
-
-    }
-
+  public function editPostMessage()
+  {
+    $mes = $_POST['mes'];
+    $mesID = $_POST['medi'];
+    $savePost = Post::find($mesID);
+    $savePost->message = $mes;
+    $savePost->save();
+    echo "Saved";
   }
-
-
 
 }
