c62a43253f9d5304e4ac98cdc868431726eba540
ferdie de oliveira <ferdie de oliveira>
2016-02-04 17:15:32 +0000

    Added closures

--- resources/views/member/home.blade.php
+++ resources/views/member/home.blade.php
@@ -5,13 +5,13 @@
   <div class="col-md-1">
     <div class="showBox red">
       <div class="showBoxTitle">POST SENT</div>
-      <div class="showBoxResult">$post->postSent</div>
+      <div class="showBoxResult">{{$post->postSent}}</div>
     </div>
   </div>
   <div class="col-md-1">
     <div class="showBox twitterBlue">
       <div class="showBoxTitle">TWEETS SENT</div>
-      <div class="showBoxResult">$post->twitterPostsSent</div>
+      <div class="showBoxResult">{{$post->twitterPostsSent}}</div>
     </div>
   </div>
 </div>
