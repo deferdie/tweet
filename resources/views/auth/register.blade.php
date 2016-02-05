99b3078c43152746e838bde0314d4b3c72dbc08e
ferdie de oliveira <ferdie de oliveira>
2016-02-05 12:00:57 +0000

    Added register styles.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,28 +1,41 @@
 @extends('master-font')
+ <section id="registration-page" class="container">
+    <form class="center" action='/auth/register' method="POST">
+      <fieldset class="registration-form">
+        <div class="control-group">
+          <!-- Username -->
+          <div class="controls">
+            <input type="text" name="name" value="{{ old('name') }}">
+          </div>
+        </div>
 
-<form method="POST" action="/auth/register" class="memberBookForm">
-{!! csrf_field() !!}
-    <div>
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
-    </div>
+        <div class="control-group">
+          <!-- E-mail -->
+          <div class="controls">
+            <input type="email" name="email" value="{{ old('email') }}">
+          </div>
+        </div>
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+        <div class="control-group">
+          <!-- Password-->
+          <div class="controls">
+            <input type="password" id="password" name="password" placeholder="Password" class="input-xlarge">
+          </div>
+        </div>
 
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
+        <div class="control-group">
+          <!-- Password -->
+          <div class="controls">
+            <input type="password" id="password_confirm" name="password_confirmation" placeholder="Password (Confirm)" class="input-xlarge">
+          </div>
+        </div>
 
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
-
-    <div>
-        <input type="submit" value="REGISTER" />
-    </div>
-</form>
+        <div class="control-group">
+          <!-- Button -->
+          <div class="controls">
+            <button class="btn btn-success btn-large btn-block">Register</button>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+  </section>
