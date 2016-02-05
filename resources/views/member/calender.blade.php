8a7a13bc24fb11216381ac9a68c2930863d042dd
ferdie de oliveira <ferdie de oliveira>
2016-02-05 15:01:06 +0000

    First Commit

---
+++ resources/views/member/calender.blade.php
@@ -0,0 +1,21 @@
+@extends('master-back')
+@section('content')
+<!-- start: Content -->
+			<div class="row-fluid sortable">
+				<div class="box span12">
+				  <div class="box-header" data-original-title>
+					  <h2><i class="halflings-icon white calendar"></i><span class="break"></span>Calendar</h2>
+				  </div>
+				  <div class="box-content">
+
+						<div id="main_calendar" class="span9">
+
+
+						</div>
+
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div><!--/row-->
+
+@endsection
