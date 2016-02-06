99711c521879a20c5a491c803a652971975671a2
ferdie de oliveira <ferdie de oliveira>
2016-02-06 22:24:01 +0000

    na

--- app/Clients.php
+++ app/Clients.php
@@ -9,6 +9,6 @@ class Clients extends Model
   //Return Client Name
   public static function clientName($clientID){
     $clientName = Clients::Find(25);
-    return $clientName->clientName;
+    //return $clientName->clientName;
   }
 }
